f83469cf0649cd5d4bda2752cf49e9bb50bba150
Cholathit Khueankaew <kcholathit@gmail.com>
2016-01-05 09:04:51 +0700

    add orderObjectBy custom filter

--- app/app.js
+++ app/app.js
@@ -223,4 +223,19 @@ angular
 
     $urlRouterProvider.otherwise('/');
   })
+
+  .filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        return (a[field] > b[field] ? 1 : -1);
+      });
+      if(reverse) filtered.reverse();
+      return filtered;
+    };
+  })
+
   .constant('FirebaseUrl', 'https://bmxyz.firebaseio.com/');
