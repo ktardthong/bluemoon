68b502192630b1d103232f365f93c5c857c5f4e6
Cholathit Khueankaew <kcholathit@gmail.com>
2016-01-11 19:20:35 +0700

    show view data in topic landing

--- app/topics/topic-landing.controller.js
+++ app/topics/topic-landing.controller.js
@@ -1,19 +1,16 @@
 angular.module('App')
-  .controller('TopicLandingCtrl', function($state,$scope, $rootScope,Slug, Topics, Auth, Users,topicLanding,replyList, viewData){
+  .controller('TopicLandingCtrl', function ($state, $scope, $rootScope, Slug, Topics, Auth, Users, topicLanding, replyList, viewData) {
+    var topicLandingCtrl = this
 
-    var topicLandingCtrl = this;
+    topicLandingCtrl.topicLanding = topicLanding
+    topicLandingCtrl.topics = Topics
+    topicLandingCtrl.replyList = replyList
+    topicLandingCtrl.views = viewData
 
-    topicLandingCtrl.topicLanding = topicLanding;
-    topicLandingCtrl.topics = Topics;
-    topicLandingCtrl.replyList = replyList;
-    topicLandingCtrl.views = viewData;
-
-    console.log(topicLandingCtrl.views);
-
-    topicLandingCtrl.incrementViews = function(topic_slug){
+    topicLandingCtrl.incrementViews = function (topic_slug) {
       console.log(topic_slug)
       console.log(topic_slug.slug)
-      //return Topics.fortopicRef(topic_slug).update({view: topic_slug.view + 1})
-    };
+    // return Topics.fortopicRef(topic_slug).update({view: topic_slug.view + 1})
+    }
 
   })
